94ab5c7a21a1251d8953a634143ce7056cdc78ae
Jackie42 <Jackie42@Lenovo-PC.fios-router.home>
2021-11-24 19:58:14 -0500

    Added method

--- src/one/DemoOne.java
+++ src/one/DemoOne.java
@@ -3,7 +3,11 @@ package one;
 public class DemoOne {
 	
 	public static void main(String[] args) {
-		System.out.println("Demo ");
+		System.out.println("Demo One");
+	}
+	
+	public static void addNum (int y, int z) {
+		System.out.println(y+z);
 	}
 
 }
